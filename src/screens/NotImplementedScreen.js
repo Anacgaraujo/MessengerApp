870e0736c8965c93ef2a878cab113b5aeea97e24
Anacgaraujo <actanaaraujo@gmail.com>
2023-05-11 20:51:08 -0400

    Initial Commit

---
+++ src/screens/NotImplementedScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function NotImplementedScreen() {
+  return (
+    <View>
+      <Text>NotImplementedScreen</Text>
+    </View>
+  );
+}
